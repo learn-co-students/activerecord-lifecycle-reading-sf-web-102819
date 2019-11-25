4c25951e1413aa7fc68ee09c2753c0bd95a9bee4
Allen Shin <geeuho@gmail.com>
2019-11-25 10:39:30 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
-
+ 
 	def show
 		@post = Post.find(params[:id])
 	end
@@ -12,21 +12,28 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params.require(:post).permit(:title, :description))
-    if @post.save
-      redirect_to post_path(@post)
-    else
-      render :new
-    end
+	  @post = Post.new(strong_params)
+	  @post.save
+	  redirect_to post_path(@post)
+    # if @post.save
+    #   redirect_to post_path(@post)
+    # else
+    #   render :new
+    # end
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params.require(:post).permit(:title, :description, :post_status, :author_id))
-	  redirect_to post_path(@post)
+	  @post.update(strong_params)
+	  redirect_to posts_path
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
+	end
+	
+	private
+	def strong_params
+		params.require(:post).permit(:title, :description)
 	end
 end
